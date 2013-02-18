0724f16a0efde6bc69ba1c71480cbbff19119ddc
Lauri <tammmeveski@gmail.com>
2013-02-18 21:42:58 +0200

    More style and html

--- Evalimine/war/menu.js
+++ Evalimine/war/menu.js
@@ -1,9 +1,11 @@
-document.write('<a href="Evalimine.html" class="menu"> Home </a>');
- 
-document.write('<a href="kandidaadid.html" class="menu"> Kandidaadid </a>');
- 
-document.write('<a href="statistika.html" class="menu"> Statistika </a>');
+document.write("<head><link rel='stylesheet' type='text/css' href='style.css'></head>");
 
-document.write('<a href="kkk.html" class="menu"> KKK </a>');
+document.write('<a href="Evalimine.html" class="menu">Esileht</a>');
  
+document.write('<a href="kandidaadid.html" class="menu">Kandidaadid</a>');
  
+document.write('<a href="statistika.html" class="menu">Statistika</a>');
+
+
+document.write('<a href="kkk.html" class="extra">KKK</a>');
+
