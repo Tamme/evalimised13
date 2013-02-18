b192ac79951effedadbfdf29ed202b0d73e9f7b2
Lauri <tammmeveski@gmail.com>
2013-02-18 15:34:47 +0200

    Added more menu, no one thta is included in all others

--- Evalimine/war/menu.js
+++ Evalimine/war/menu.js
@@ -1,25 +1,9 @@
-document.write('<ul class="menu">');
+document.write('<a href="Evalimine.html" class="menu"> Home </a>');
+ 
+document.write('<a href="kandidaadid.html" class="menu"> Kandidaadid </a>');
+ 
+document.write('<a href="statistika.html" class="menu"> Statistika </a>');
 
-document.write('<li>');
-document.write('<a href="index.html" class="menu">Home</a>');
+document.write('<a href="kkk.html" class="menu"> KKK </a>');
  
-document.write('</li><li>');
-document.write('<a href="about.html" class="menu">About Us</a>');
  
-document.write('</li><li>');
-document.write('<a href="services.html" class="menu">Services</a>');
- 
-document.write('</li><li>');
-document.write('<a href="gallery.html" class="menu">Photo Gallery</a>');
- 
-document.write('</li><li>');
-document.write('<a href="testimonials.html" class="menu">Testimonials</a>');
- 
-document.write('</li><li>');
-document.write('<a href="contact.html" class="menu">Contact Us</a>');
- 
-document.write('</li>');
- 
-// END LINKS //
- 
-document.write('</ul>');
