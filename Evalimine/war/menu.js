c2ca0d500c0d5fada5fc777aa6773aff1708d50d
Lauri <tammmeveski@gmail.com>
2013-02-18 15:23:27 +0200

    Added some menu

---
+++ Evalimine/war/menu.js
@@ -0,0 +1,25 @@
+document.write('<ul class="menu">');
+
+document.write('<li>');
+document.write('<a href="index.html" class="menu">Home</a>');
+ 
+document.write('</li><li>');
+document.write('<a href="about.html" class="menu">About Us</a>');
+ 
+document.write('</li><li>');
+document.write('<a href="services.html" class="menu">Services</a>');
+ 
+document.write('</li><li>');
+document.write('<a href="gallery.html" class="menu">Photo Gallery</a>');
+ 
+document.write('</li><li>');
+document.write('<a href="testimonials.html" class="menu">Testimonials</a>');
+ 
+document.write('</li><li>');
+document.write('<a href="contact.html" class="menu">Contact Us</a>');
+ 
+document.write('</li>');
+ 
+// END LINKS //
+ 
+document.write('</ul>');
