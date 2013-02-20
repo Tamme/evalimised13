db7720efa2b613145350b23e2209b6ee32a5f977
Lauri <tammmeveski@gmail.com>
2013-02-20 21:46:52 +0200

    front page basically ready

--- Evalimine/war/menu.js
+++ Evalimine/war/menu.js
@@ -1,6 +1,6 @@
 document.write("<head><link rel='stylesheet' type='text/css' href='style.css'></head>");
 
-document.write('<a href="Evalimine.html" class="menu">Esileht</a>');
+document.write('<a href="Evalimine.html" class="proov">Esileht</a>');
  
 document.write('<a href="kandidaadid.html" class="menu">Kandidaadid</a>');
  
