17162bc1b6b2df29f297f38efd0c9412f096efeb
Lauri <tammmeveski@gmail.com>
2013-02-22 20:19:16 +0200

    Added search to menu

--- Evalimine/war/menu.js
+++ Evalimine/war/menu.js
@@ -1,13 +1,15 @@
+
 document.write("<head><link rel='stylesheet' type='text/css' href='style.css'></head>");
 
-document.write('<a href="Evalimine.html" class="proov">Esileht</a>');
+document.write('<a id="esi" href="Evalimine.html">Esileht</a>');
  
-document.write('<a href="kandidaadid.html" class="menu">Kandidaadid</a>');
+document.write('<a href="kandidaadid.html">Kandidaadid</a>');
  
-document.write('<a href="statistika.html" class="menu">Statistika</a>');
+document.write('<a href="statistika.html" title="Hello" class="menu">Statistika</a>');
 
 
-document.write('<a href="kkk.html" class="extra">KKK</a>');
+document.write('<a href="kkk.html">KKK</a>');
 
-document.write('<a href="enter.html" class="extra">Sisene</a>');
+document.write('<a href="enter.html" >Sisene</a>');
 
+//document.write('<form class="searchbar"><input type="text" name=search"><input class="btn" type="submit" value="Otsi" name="search_btn"/></form>');
