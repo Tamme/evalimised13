9d59c95e680aede0096c379e8d590c187d884934
Siimu <Siimu@Siimu-PC.lan>
2013-02-24 20:40:35 +0200

    Created JavaScript sub-menu to statistika. Added sub-menu to statistika.html. Added statkandidaat.html, statpartei.html, statpiirkonna.html. Added statmenu to CSS and changed searchbar CSS.

---
+++ Evalimine/war/statmenu.js
@@ -0,0 +1,13 @@
+document.write("<head><link rel='stylesheet' type='text/css' href='style.css'></head>");
+
+document.write('<a href="statistika.html" >Statistika riigi l&#245;ikes</a>');
+ 
+document.write('<a href="statpiirkonna.html" >Statistika piirkondade l&#245;ikes</a>');
+
+document.write('<a href="statpartei.html" >Statistika parteide l&#245;ikes</a>');
+ 
+document.write('<a href="statkandidaat.html" >Statistika kandidaatide l&#245;ikes</a>');
+
+
+
+
