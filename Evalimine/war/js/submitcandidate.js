1beb8c529c3d010a65bda85c6f5f880e2c8213fc
Madis Masso <madismasso@gmail.com>
2013-03-10 21:59:35 +0200

    Dummy submitcandidate .js

---
+++ Evalimine/war/js/submitcandidate.js
@@ -0,0 +1,10 @@
+﻿	function kandideerimisFunktsioon() {
+		var x;
+		var r=confirm("Kas soovid tõesti kandideerida?");
+		if (r==true) {
+				x="Olete kandideeritud!";
+  		}
+		else {
+  			x="Kandideerimine tühistatud!";
+  		}
+	}
