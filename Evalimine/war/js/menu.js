aec08f8a5a036dcae907f58d8a826982a4d1bbc5
Madis Masso <madismasso@gmail.com>
2013-03-11 11:55:54 +0200

    Global cleanup, removed the search bar, extended the body size, updated javascript and image file locations

--- Evalimine/war/js/menu.js
+++ Evalimine/war/js/menu.js
@@ -7,9 +7,10 @@ document.write('<a href="kandidaadid.html">Kandidaadid</a>');
  
 document.write('<a href="statistika.html" title="Hello" class="menu">Statistika</a>');
 
-
 document.write('<a href="kkk.html">KKK</a>');
 
 document.write('<a href="enter.html" >Sisene</a>');
+
+document.write('<a href="minuandmed.html" >Minu andmed</a>');
 
 //document.write('<form class="searchbar"><input type="text" name=search"><input class="btn" type="submit" value="Otsi" name="search_btn"/></form>');
